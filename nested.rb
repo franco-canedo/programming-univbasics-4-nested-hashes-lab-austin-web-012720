21b95cf4607218060f38cf295bf166c135ff8f27
Franco Canedo <francanedo1@hotmail.com>
2020-01-07 21:12:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,8 +86,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-new_hash = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+#new_hash = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 puts programmer_hash
     
 end
